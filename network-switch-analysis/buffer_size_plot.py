86f81fc448f3a1132ebce9f1780883c5572a750d
Dmitriy Plaks <GTpyro@users.noreply.github.com>
2026-02-20 13:26:13 -0600

    added a streamlit app for the buffer size plot

---
+++ network-switch-analysis/buffer_size_plot.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+data_url = "https://github.com/GTpyro/knowledge/raw/refs/heads/main/network-switch-analysis/Network%20Switch%20Performance.xlsx"
+
+@st.cache_data
+def get_data(data_url: str) -> pd.DataFrame:
+    return pd.read_excel(data_url)
+
+switch_metrics = get_data(data_url)
+switch_metrics["Total Port Count"] = (
+    switch_metrics["port 1 count"]
+    + switch_metrics["port 2 count"]
+    + switch_metrics["port 3 count"]
+    + switch_metrics["port 4 count"]
+)
+switch_metrics["Expected Switching Capacity"] = (
+    switch_metrics["port 1 count"] * switch_metrics["port 1 speed"]
+    + switch_metrics["port 2 count"] * switch_metrics["port 2 speed"]
+    + switch_metrics["port 3 count"] * switch_metrics["port 3 speed"]
+    + switch_metrics["port 4 count"] * switch_metrics["port 4 speed"]
+) / 1000
+switch_metrics["Average Port Speed (Gbps)"] = (
+    switch_metrics["Expected Switching Capacity"] / switch_metrics["Total Port Count"]
+)
+switch_metrics["Time to Fill Buffer (s)"] = (
+    switch_metrics["Buffer Size (MB)"]
+    * 8
+    / switch_metrics["Average Port Speed (Gbps)"]
+    / 1000
+)
+
+fig6 = px.scatter(
+    switch_metrics,
+    x="Time to Fill Buffer (s)",
+    y="Buffer Size (MB)",
+    title="Network Switch Comparison",
+    color="Manufacturer",
+    labels={
+        "Time to Fill Buffer (s)": "Round Trip Time (s)",
+    },
+    # size="Total Port Count",
+    # symbol="Total Port Count",
+    hover_data=["Model", "Switching Capacity (Gbps)"],
+    log_x=True,
+    log_y=True,
+)
+st.plotly_chart(fig6)
